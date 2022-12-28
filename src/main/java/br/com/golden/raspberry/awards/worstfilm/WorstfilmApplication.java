131954d0a65d6b79008f94d0aeb62b6193b8a19b
Ricardo Soares <riicardo.soares@gmail.com>
2022-12-28 14:47:36 -0300

    Fixed the gitignore

---
+++ src/main/java/br/com/golden/raspberry/awards/worstfilm/WorstfilmApplication.java
@@ -0,0 +1,13 @@
+package br.com.golden.raspberry.awards.worstfilm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorstfilmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorstfilmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/golden/raspberry/awards/worstfilm/WorstfilmApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.golden.raspberry.awards.worstfilm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorstfilmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
